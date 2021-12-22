c00723ae3731a8459d978ab4d9efe679bdf99480
JoaoTonet <jh.tonet@gmail.com>
2021-12-22 20:41:04 -0300

    feat: insert new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,8 +2,6 @@ import { FormEvent, useState, useContext } from 'react';
 import { TransactionsContext } from '../../TransactionsContext';
 import Modal from 'react-modal';
 
-import { api } from '../../services/api';
-
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -26,15 +24,21 @@ export function NewTransactionModal({
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     });
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
   }
 
   return (
